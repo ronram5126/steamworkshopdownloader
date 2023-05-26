4256d928850dacdfcefb9a1a9f2da762b659bc0e
aprox2 <edge15831@gmail.com>
2023-05-26 21:34:50 +0300

    Fixed some stuff. Should work now

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const jsdom = require("jsdom");
 const axios = require("axios").default;
 const { DownloaderHelper }  = require("node-downloader-helper");
+const cheerio = require('cheerio');
 
 const { JSDOM } = jsdom;
 
@@ -32,13 +33,13 @@ async function download(count = 0) {
     }
     if (url.includes("http"))
     await axios.get(url).then(async ({data}) => {
-        
+
         const dom = new JSDOM(data);
         const window = dom.window;
         const document = window.document;
         const linkArray = [].slice.call(document.getElementsByClassName("collectionItemDetails")).map(a => a.children[0]);
         const namedUrlArray = linkArray.map(link => [link.href, link.children[0].textContent])
-        
+
         const collectionName = document.getElementsByClassName("workshopItemTitle")[0].textContent;
         const collectionDir = `${downloadDir}/${validName(collectionName)}`;
 
@@ -51,15 +52,15 @@ async function download(count = 0) {
             .getElementsByTagName('a')[0]
             .href.split('/')
         );
-        
+
         console.log(`downloading ${collectionName}`);
-        
+
         for (let idx = 0; idx < namedUrlArray.length; idx ++) {
             const [link, packageName] = namedUrlArray[idx];
             const workshopId = link.split("?id=")[1];
             const url = `http://steamworkshop.download/download/view/${workshopId}`;
             const fileName =  validName(`${packageName.split("/").join("")}${process.argv[2]||".zip"}`);
-            
+
             if (fs.existsSync(collectionDir + '/' + fileName)){
                 console.log(`Skipping ${packageName.split("/").join("")}, file already downloaded`)
 
@@ -71,14 +72,27 @@ async function download(count = 0) {
 
                     const subDownloadButton = document.getElementById("steamdownload");
 
-                    
+
                     // const filePath = `${collectionDir}/${fileName}`
-                    
+
                     console.log(`    ${idx}. ${packageName}`);
                     if (subDownloadButton) {
-                        let response = await axios.post("http://steamworkshop.download/online/steamonline.php", `item=${workshopId}&app=${appId}`);
+                        console.log("http://steamworkshop.download/online/steamonline.php", `item=${workshopId}&app=${appId}`)
+                        const data = {
+                            item: workshopId,
+                            app: appId,
+                        }
+                        let response = await axios.get(`http://steamworkshop.download/download/view/${workshopId}`);
+                        const html = response.data;
+
+                        const $ = cheerio.load(html);
+                        const links = [];
+                        $('a').each((index, element) => {
+                            const href = $(element).attr('href');
+                            links.push(href);
+                        });
                         const url = last(response.data.split("<a href='")).split("'>")[0];
-                        await downloadAndSave(url, collectionDir, fileName);
+                        await downloadAndSave(links[5], collectionDir, fileName);
                     } else {
                         const link = document.getElementsByTagName("table")[0].children[0].children[0].children[0].children[1].children[0];
                         const url = link.href;
@@ -92,7 +106,7 @@ async function download(count = 0) {
                 });
             }
         };
-        
+
 
     }).catch(err => {
         console.log(url);
